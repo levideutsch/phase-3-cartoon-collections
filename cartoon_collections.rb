9b6a7be1aa707442d7f7841037f53084004807be
levideutsch <levideutsch@live.com>
2023-02-24 15:11:14 -0500

    commit tree 10f0e5191f16f134d5b56fd5fc0549619e13e306

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,48 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(array)
+  array.map.with_index do |item, index|
+    puts "#{index + 1}. #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+# upcase = array.map(&:upcase)
+# return upcase
+# final = upcase
+# array.map! {|word| "#{word}!"}
+one = array.map { | item | item[0].upcase + item[1..-1] }
+one.map! {|word| "#{word}!"}
+return one
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+ array.any? do |word|
+  if word.length > 4 
+    true
+  elsif word.length <= 4
+    false
+  end 
+end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.find do |item|
+    if cheese_types.include?(item)
+      item
+    else 
+      nil
+    end
+  end
+
+  # array.find do |item|
+  #   if item.include?(cheese_types) 
+  #     return item
+  #   else 
+  #     return nil
+  #   end 
+  # end 
+  # binding.pry
 end
